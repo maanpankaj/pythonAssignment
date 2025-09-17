cce5022a706489cf03e83933058c88e89c71dfbf
maanpankaj <109466455+maanpankaj@users.noreply.github.com>
2025-09-17 22:20:51 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,24 @@
+# Task 1
+
+
+a=input("Enter the first number:")
+b=input("Enter the second number:")
+a = int(a)
+b = int(b)
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print("The sum is:",c)
+print("The difference is:",d)
+print("The product is:",e)
+print("The quotient is:",f)
+
+
+# Task 2
+a = input("Enter your first name:")
+b = input("Enter your second name:")
+
+c = a +" " +b
+
+print("Hello",c , "welcome to python programming")
